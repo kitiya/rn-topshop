61c5ad275325612a8b05aaaaa213415988d892a2
Kitiya <kate.kitiya@gmail.com>
2020-06-28 00:52:00 -0400

    165. Adding Touchable component

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,28 +1,48 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import {
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  Platform,
+} from "react-native";
 import Colors from "../../constants/Colors";
 
 const ProductItem = (props) => {
+  let TouchableComponent = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21)
+    TouchableComponent = TouchableNativeFeedback;
+
   return (
     <View style={styles.productContainer}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: props.image }} />
-      </View>
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.actionsContainer}>
-        <Button
-          color={Colors.primary}
-          title="View Details"
-          onPress={props.onViewDetail}
-        />
-        <Button
-          color={Colors.primary}
-          title="To Cart"
-          onPress={props.onAddToCart}
-        />
+      <View style={styles.touchableContainer}>
+        <TouchableComponent onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.detailsContainer}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actionsContainer}>
+              <Button
+                color={Colors.primary}
+                title="View Details"
+                onPress={props.onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title="To Cart"
+                onPress={props.onAddToCart}
+              />
+            </View>
+          </View>
+        </TouchableComponent>
       </View>
     </View>
   );
@@ -39,6 +59,10 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.light,
     height: 300,
     margin: 20,
+  },
+  touchableContainer: {
+    overflow: "hidden",
+    borderRadius: 10,
   },
   imageContainer: {
     width: "100%",
--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -3,11 +3,13 @@ import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
   },
   {
     defaultNavigationOptions: {
