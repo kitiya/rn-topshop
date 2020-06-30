4fb3de24842e6f290d144e882a57c00aafe0f166
Kitiya <kate.kitiya@gmail.com>
2020-06-30 18:17:15 -0400

    179. Deleting a Product item

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,6 +1,8 @@
 import PRODUCTS from "../../data/dummy-data";
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
+import { DELETE_PRODUCT } from "../actions/products";
 import { ADD_ORDER } from "../actions/orders";
+
 import CartItem from "../../models/cart-item";
 
 const initialState = {
@@ -73,6 +75,21 @@ export default (state = initialState, action) => {
         ...state,
         ...initialState,
       };
+
+    case DELETE_PRODUCT: {
+      if (!state.items[action.pid]) {
+        return state;
+      }
+
+      const updatedItems = { ...state.items };
+      const itemTotal = state.items[action.pid].sum;
+      delete updatedItems[action.pid];
+      return {
+        ...state,
+        items: updatedItems,
+        totalAmount: state.totalAmount - itemTotal,
+      };
+    }
   }
   return state;
 };
