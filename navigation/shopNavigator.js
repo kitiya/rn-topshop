0b5ade28405378f97a8e2c38764d11e8e6483bd7
Kitiya <kate.kitiya@gmail.com>
2020-06-29 20:45:14 -0400

    177. Building the 'UserProductsScreen

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -9,6 +9,7 @@ import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
+import UserProductsScreen from "../screens/user/UserProductsScreen";
 import Colors from "../constants/Colors";
 
 const defaultNavOptions = {
@@ -60,10 +61,27 @@ const OrdersNavigator = createStackNavigator(
   }
 );
 
+const AdminNavigator = createStackNavigator(
+  { UserProducts: UserProductsScreen },
+  {
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-create" : "ios-create"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
 const ShopNavigator = createDrawerNavigator(
   {
     Products: ProductsNavigator,
     Orders: OrdersNavigator,
+    Admin: AdminNavigator,
   },
   {
     contentOptions: {
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -2,7 +2,7 @@ import PRODUCTS from "../../data/dummy-data";
 
 const initialState = {
   availableProducts: PRODUCTS,
-  userProducts: PRODUCTS.filter((product) => product.owerId === "u1"),
+  userProducts: PRODUCTS.filter((product) => product.ownerId === "u1"),
 };
 
 export default (state = initialState, action) => {
