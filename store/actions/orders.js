1ca8e858212d1ad599c3b6aada3fae6f75a33044
Kitiya <kate.kitiya@gmail.com>
2020-07-04 06:31:56 -0400

    206. Storing orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,11 +1,37 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: {
-      items: cartItems,
-      amount: totalAmount,
-    },
+  return async (dispatch) => {
+    const date = new Date();
+    const response = await fetch(
+      "https://rn-topshop.firebaseio.com/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Error on creating an order.");
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
