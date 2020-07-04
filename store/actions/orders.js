60c979d6de66e472785e70dfb4b02ccf3b8e55de
Kitiya <kate.kitiya@gmail.com>
2020-07-04 07:08:09 -0400

    208. Fetching store orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,42 @@
+import Order from "../../models/order";
+
 export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDER = "SET_ORDER";
+
+export const fetchOrders = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://rn-topshop.firebaseio.com/orders/u1.json"
+      );
+
+      // check response status before unpack
+      if (!response.ok) {
+        throw new Error("Error on fetching orders!");
+      }
+
+      const resData = await response.json();
+
+      // convert object to array
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+
+      dispatch({ type: SET_ORDER, orders: loadedOrders });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -1,4 +1,4 @@
-import { ADD_ORDER } from "../actions/orders";
+import { ADD_ORDER, SET_ORDER } from "../actions/orders";
 import Order from "../../models/order";
 
 const initialState = {
@@ -7,6 +7,11 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_ORDER:
+      return {
+        ...state,
+        orders: action.orders,
+      };
     case ADD_ORDER:
       const newOrder = new Order(
         action.id,
