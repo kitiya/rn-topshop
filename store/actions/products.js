d5f5f6113c2a90d152de6416cc9cd4ba890ffa3e
Kitiya <kate.kitiya@gmail.com>
2020-07-04 05:51:56 -0400

    204. Updating & deleting products

--- store/actions/products.js
+++ store/actions/products.js
@@ -45,9 +45,18 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return {
-    type: DELETE_PRODUCT,
-    pid: productId,
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-topshop.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    dispatch({
+      type: DELETE_PRODUCT,
+      pid: productId,
+    });
   };
 };
 
@@ -71,7 +80,7 @@ export const createProduct = (title, description, imageUrl, price) => {
     );
 
     const resData = await response.json();
-    // console.log(resData); // product id
+    // console.log(resData); // returns product id
 
     // Redux Thunk will dispatch a new action object for you
     dispatch({
@@ -88,13 +97,27 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(`https://rn-topshop.firebaseio.com/products/${id}.json`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+      }),
+    });
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
