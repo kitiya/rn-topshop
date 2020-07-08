409b3903d0ce7436b02036f09a103eec150ccefa
Kitiya <kate.kitiya@gmail.com>
2020-07-08 13:50:19 -0400

    288. Remaining migration steps & roundup

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -13,7 +13,7 @@ import * as cartActions from "../../store/actions/cart";
 import Colors from "../../constants/Colors";
 
 const ProductDetailScreen = (props) => {
-  const productId = props.navigation.getParam("productId");
+  const productId = props.route.params.productId;
   const selectedProduct = useSelector((state) =>
     state.products.availableProducts.find((prod) => prod.id === productId)
   );
@@ -40,7 +40,7 @@ const ProductDetailScreen = (props) => {
 
 export const screenOptions = (navData) => {
   return {
-    headerTitle: navData.navigation.getParam("productTitle"),
+    headerTitle: navData.route.params.productTitle,
   };
 };
 
